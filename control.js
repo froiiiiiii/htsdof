8ed4d4407e75c35babb623fbb841a1a78ed634c5
froiiiiiii <fgonzalez8444@utm.edu.ec>
2026-01-28 20:54:12 -0500

    cambiando estilos de la primera seccion

--- control.js
+++ control.js
@@ -72,7 +72,6 @@ function activeNavItem(item, update=true) {
 
 function changeUrl(name){
     const nuevaRuta =  `index.html#${name}`;
-    console.log("cambiando a index");
     
     history.replaceState({}, "", nuevaRuta);
 }
